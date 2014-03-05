1145aff42df515ffc9d269a1a9713ae7fd72fff5
juanmt1989 <juanmt1989@gmail.com>
2014-03-05 10:42:37 -0500

    adding janusgrid and valuelist in the customerid column.

--- JanusGridExEjmp2/JanusGridExEjmp2/Form1.Designer.cs
+++ JanusGridExEjmp2/JanusGridExEjmp2/Form1.Designer.cs
@@ -29,11 +29,81 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            Janus.Windows.GridEX.GridEXLayout gridEX1_DesignTimeLayout = new Janus.Windows.GridEX.GridEXLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gridEX1 = new Janus.Windows.GridEX.GridEX();
+            this.invoicesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.northwindDataSet = new JanusGridExEjmp2.northwindDataSet();
+            this.invoicesTableAdapter = new JanusGridExEjmp2.northwindDataSetTableAdapters.InvoicesTableAdapter();
+            this.CustomersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.customersTableAdapter = new JanusGridExEjmp2.northwindDataSetTableAdapters.CustomersTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.gridEX1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invoicesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridEX1
+            // 
+            this.gridEX1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridEX1.DataSource = this.invoicesBindingSource;
+            gridEX1_DesignTimeLayout.LayoutString = resources.GetString("gridEX1_DesignTimeLayout.LayoutString");
+            this.gridEX1.DesignTimeLayout = gridEX1_DesignTimeLayout;
+            this.gridEX1.Location = new System.Drawing.Point(12, 50);
+            this.gridEX1.Name = "gridEX1";
+            this.gridEX1.Size = new System.Drawing.Size(608, 376);
+            this.gridEX1.TabIndex = 0;
+            // 
+            // invoicesBindingSource
+            // 
+            this.invoicesBindingSource.DataMember = "Invoices";
+            this.invoicesBindingSource.DataSource = this.northwindDataSet;
+            // 
+            // northwindDataSet
+            // 
+            this.northwindDataSet.DataSetName = "northwindDataSet";
+            this.northwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // invoicesTableAdapter
+            // 
+            this.invoicesTableAdapter.ClearBeforeFill = true;
+            // 
+            // CustomersBindingSource
+            // 
+            this.CustomersBindingSource.DataMember = "Customers";
+            this.CustomersBindingSource.DataSource = this.northwindDataSet;
+            // 
+            // customersTableAdapter
+            // 
+            this.customersTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(632, 438);
+            this.Controls.Add(this.gridEX1);
+            this.Name = "Form1";
+            this.Text = "Adding ValueList in GidEx ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridEX1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invoicesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Janus.Windows.GridEX.GridEX gridEX1;
+        private northwindDataSet northwindDataSet;
+        private System.Windows.Forms.BindingSource invoicesBindingSource;
+        private northwindDataSetTableAdapters.InvoicesTableAdapter invoicesTableAdapter;
+        private System.Windows.Forms.BindingSource CustomersBindingSource;
+        private northwindDataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
     }
 }
 
--- JanusGridExEjmp2/JanusGridExEjmp2/Form1.cs
+++ JanusGridExEjmp2/JanusGridExEjmp2/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Janus.Windows.GridEX;
 
 namespace JanusGridExEjmp2
 {
@@ -14,6 +15,54 @@ namespace JanusGridExEjmp2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'northwindDataSet.Customers' Puede moverla o quitarla según sea necesario.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+            // TODO: esta línea de código carga datos en la tabla 'northwindDataSet.Invoices' Puede moverla o quitarla según sea necesario.
+            this.invoicesTableAdapter.Fill(this.northwindDataSet.Invoices);         
+            FillCategoriesValueList();
+        }
+
+        private void FillCategoriesValueList()
+        {
+            //Get the CategoryID column
+            GridEXColumn column = gridEX1.RootTable.Columns["CustomerID"];
+           
+            //Set HasValueList property equal to true in order to be able to use the ValueList property
+            column.HasValueList = true;
+            //Get the ValueList collection associated to this column
+            GridEXValueListItemCollection valueList = column.ValueList;
+
+            //Fill the ValueList           
+            //valueList.PopulateValueList(northwindDataSet.Customers.DefaultView,"CustomerID", "CompanyName");
+
+            //An alternative way to fill the value list is using the Add method as follows:
+
+
+            DataView view = northwindDataSet.Customers.DefaultView;
+            foreach(DataRowView row in view)
+            {
+                valueList.Add(row["CustomerID"],(string)row["CompanyName"]);
+            }
+            
+
+
+            //Setting other column related properties
+
+            //When using a value list you could use DropDownList and Combo EditType 
+            //in the column and the values for the dropdown list will be the values
+            //in the ValueList collection
+            column.EditType = EditType.DropDownList;
+            //To be able to sort using the replaced value and not the value in the
+            //CategoryID field change the CompareTarget property to Text instead of value
+            column.CompareTarget = ColumnCompareTarget.Text;
+
+            //Likewise, to group by the replaced text do:
+            column.DefaultGroupInterval = GroupInterval.Text;
+
         }
     }
 }
